e98cbfcb70def59964a3ff5620311e0f3f04d45b
mario-heleno <mmariohelenosilva@gmail.com>
2023-11-17 22:31:37 -0300

    feat: Implementação RNN

--- model_predict.py
+++ model_predict.py
@@ -1,23 +1,37 @@
 from services.noticia_service import NoticiaService
 from utls.text_help import clean_text
+from keras.models import load_model
 from aws.s3_aux import S3Aux
 import pandas as pd
-import joblib
-
+from joblib import load
+import spacy
+import re
 
 # Modelos
-modelo = joblib.load('models/random_forest_model_news.pkl')
-modelo_vec = joblib.load('models/count_vectorizer_model_news.pkl')
+modelo_predicao_rnn = load_model('models/model_rnn.h5')
+modelo_pad_sequences = load('models/padsequences.pkl')
+modelo_tokenizer = load('models/tokenizer.pkl')
 
 
-def predict_value(valor):
-    previsao = modelo.predict(valor)
-    return previsao
+def lematize_sentence(sentence):
+    nlp = spacy.load("pt_core_news_sm")
+    doc = nlp(sentence)
+    return ' '.join([word.lemma_ for word in doc])
 
 
-def vectorize_value(valor):
-    valor_vec = modelo_vec.transform([valor])
-    return valor_vec
+def limpar_texto(value):
+    value = re.sub(r"(#\S+)|(@\S+)|(http\S+)", "", value)
+    for char in ['.', ';', '-', ':', ')']:
+        value = value.replace(char, '')
+    return value.strip().lower()
+
+
+def texts_to_sequences(value):
+    return modelo_tokenizer.texts_to_sequences([value])
+
+
+def pad_seq(value):
+    return modelo_pad_sequences(value, maxlen=300)
 
 
 bucket_name = 'seekers-bucket'
@@ -31,12 +45,15 @@ df = pd.read_csv(local_filename)
 
 df['texto_noticia'] = df['texto_noticia'].str.lower()
 df['texto_tratado'] = df['texto_noticia'].apply(clean_text)
-df['texto_vetorizado'] = df['texto_tratado'].apply(vectorize_value)
-df['resultado'] = df['texto_vetorizado'].apply(predict_value)[0][0]
+df['texto_vetorizado'] = df['texto_tratado'].apply(limpar_texto)
+df['lematize_txt'] = df['texto_vetorizado'].apply(lematize_sentence)
+df['seq'] = df['lematize_txt'].apply(texts_to_sequences)
+df['padded_seq'] = df['seq'].apply(pad_seq)
+df['resultado'] = df['padded_seq'].apply(modelo_predicao_rnn.predict)
 
 ns = NoticiaService()
 
 for index, row in df.iterrows():
-    resultado = row['resultado'] == 'true'
+    resultado = 1 if row['resultado'][0][0] >= 0.5 else 0
     link = row['link']
     ns.inserir_resultado_noticia(resultado, link)
